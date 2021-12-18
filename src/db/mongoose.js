86b5329039c2a2ddddc68a56e697a599d8943f7a
Mavi <jomavix@hotmail.com>
2021-12-18 11:35:45 +0100

    corrected assigned MONGO_URI to uri

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-// process.env.MONGO_URI ||
-const uri =
+const uri = process.env.MONGO_URI
 mongoose.connect(uri, { useNewUrlParser: true }, (err) => {
   if (err) console.error('Error with MongoDB connection:', err)
   else console.log('MongoDB connection established successfully')
